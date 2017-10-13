4285baefeeede6fa0f6c2d5d27ac357a843c71b7
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-12 20:36:04 -0400

    Fixed the path in travel.log file to get the absolute dir and get the file.

--- src/csvParser/travel/csv_parser.py
+++ src/csvParser/travel/csv_parser.py
@@ -2,14 +2,16 @@
 # -*- coding: UTF-8 -*-
 """Parser CSV with csv module"""
 
+import os
 import csv
 
 
 def csv_parser():
     """This function open the log file and split by commas, then it count the total distances traveled"""
     total_distance = 0
+    dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    file_id = open("./travel/travel.log")
+    file_id = open(dir_path + "/travel.log")
     content = file_id.readlines()
     file_id.close()
 
--- src/csvParser/travel/split_parser.py
+++ src/csvParser/travel/split_parser.py
@@ -2,12 +2,15 @@
 # -*- coding: UTF-8 -*-
 """Parser CSV via split built-in function"""
 
+import os
+
 
 def split_parser():
     """This function open the log file and split by commas, then it count the total distances traveled"""
     total_distance = 0
+    dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    file_id = open("./travel/travel.log")
+    file_id = open(dir_path + "/travel.log")
     content = file_id.readlines()
     file_id.close()
 
